c9047418230fa9a9d5bc9dae047a239baf68a577
Kidus Mamuye Tekeste <kidusmamuye@gmail.com>
2021-08-11 00:12:21 -0500

    registered application in Eureka Server

--- src/main/java/project/swa/CustomerService/CustomerCommandApplication.java
+++ src/main/java/project/swa/CustomerService/CustomerCommandApplication.java
@@ -2,10 +2,14 @@ package project.swa.CustomerService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @EnableKafka
+@EnableFeignClients
+@EnableDiscoveryClient
 public class CustomerCommandApplication {
 
 	public static void main(String[] args) {
